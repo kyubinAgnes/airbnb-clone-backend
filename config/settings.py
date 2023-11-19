7a024fae44b3a0c990192fe1367271c427d668fd
inkblot626 <inkblot626@gmail.com>
2023-11-19 11:09:09 +0900

    create Rvviews

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,9 @@ CUSTOM_APPS = ["common.apps.CommonConfig",
                "users.apps.UsersConfig", 
                "rooms.apps.RoomsConfig", 
                 "experiences.apps.ExperiencesConfig",
-                "categories.apps.CategoriesConfig"]
+                "categories.apps.CategoriesConfig",
+                "reviews.apps.ReviewsConfig",
+                ]
 
 
 SYSTEM_APPS = [
