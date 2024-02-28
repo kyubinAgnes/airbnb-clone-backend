01ae715652678b104aaca17edd7f878590d6d6db
inkblot626 <inkblot626@gmail.com>
2024-02-28 22:35:09 +0900

    kakao

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("log-out", views.LogOut.as_view()),
     path("jwt-login", views.JWTLogIn.as_view()),
     path("github", views.GithubLogIn.as_view()),
+    path("kakao", views.KakaoLogIn.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
 ]
